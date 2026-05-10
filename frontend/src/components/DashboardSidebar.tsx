65221e17de40a00fee41b371d5f0efd0562618b1
Mansurov Akshin <mansurov1asus@mail.ru>
2026-05-10 18:25:58 +0400

    fix: remove unused import

--- frontend/src/components/DashboardSidebar.tsx
+++ frontend/src/components/DashboardSidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation, useNavigate } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { cn } from "../lib/utils"
 import { Button } from "../components/ui/button"
 import {
